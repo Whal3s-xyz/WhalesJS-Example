af9915c5cba7d3583413d6b50abc92e54e85be31
Jonas Esser <mail@jonas-esser.de>
2022-09-08 17:41:16 +0200

    Redesigned Demo

---
+++ src/components/EventCondition.vue
@@ -0,0 +1,276 @@
+<script>
+import Whal3s from 'whal3sjs'
+
+export default {
+  data() {
+    return {
+      log: [],
+      utility: null,
+
+      engagements: {},
+      user: {
+        isValid: false,
+        engagements: {},
+        maxEngagementsReached: false
+      }
+    }
+  },
+  created() {
+    // fetch on init
+    const whal3s = new Whal3s();
+    this.utility = whal3s.getUtility('46162418-800b-4487-a77d-04f6a5fc22dd')
+    this.utility.init()
+    this.utility.addEventListener('initialized', () => this.log.push('initialized'))
+    this.utility.addEventListener('initializationFailed', () => this.log.push('initializationFailed'))
+    this.utility.addEventListener('networkSwitch', () => this.log.push('networkSwitch'))
+    this.utility.addEventListener('networkSwitchFailed', () => this.log.push('networkSwitchFailed'))
+    this.utility.addEventListener('sending', () => this.log.push('sending'))
+    this.utility.addEventListener('sent', () => this.log.push('sent'))
+    this.utility.addEventListener('transactionHash', (transactionHash) => this.log.push('transactionHash: ' + transactionHash))
+    this.utility.addEventListener('confirmation', (data) => this.log.push('confirmation: ' + data.receipt))
+    this.utility.addEventListener('error', (error) => this.log.push('error: ' + error.message))
+    this.utility.addEventListener('done', (transactionHash) => this.log.push('done: ' + transactionHash))
+    this.utility.addEventListener('estimateGasError', () => this.log.push("Cannot estimate gas. Maybe token balance to low."))
+    this.utility.addEventListener('gasPriceError', () => this.log.push("Cannot fetch gas price"))
+
+    this.fetchEngagements()
+
+
+  },
+  computed: {
+    selectedAccount() {
+      return this.utility?.selectedAccount;
+    }
+  },
+  methods: {
+    async connectWallet() {
+      await this.utility.connectWallet()
+          .catch(function (error) {
+            console.log({error: error})
+          })
+    },
+    async triggerTransaction() {
+      this.utility.performAction().catch(function (error) {
+        console.log(error.code)
+        if (error.code === 3)
+          alert('Not enough token')
+        console.log(error)
+      })
+    },
+
+    async fetchEngagements() {
+      const engagements = await this.utility.fetchEngagements()
+      console.log(engagements)
+      this.engagements = engagements
+    }
+
+
+  },
+  watch: {
+    selectedAccount: function (value) {
+      if (value)
+        this.utility.validateWalletAddress()
+            .then((reponse) => {
+              this.user.isValid = reponse.valid
+              this.user.maxEngagementsReached = reponse.maxEngagementsReached
+              this.user.engagements = reponse.engagements
+            })
+      else
+        this.isValid = false
+    }
+  }
+}
+</script>
+
+<template>
+
+  <main>
+    <div class="min-h-full">
+      <div class="bg-gray-800 pb-32">
+        <nav class="bg-gray-800">
+          <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="border-b border-gray-700">
+              <div class="flex items-center justify-between h-16 px-4 sm:px-0">
+                <div class="flex items-center">
+                  <div class="flex-shrink-0">
+                    <img class="w-16" src="/logo.png"
+                         alt="Whal3s">
+                  </div>
+                </div>
+
+              </div>
+            </div>
+          </div>
+
+        </nav>
+        <header class="py-10">
+          <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 ">
+            <div class="grid grid-cols-12">
+              <div class="col-span-12 md:col-span-6">
+              <h1 class="text-3xl font-bold text-white">Onchain Events</h1>
+                <p class="text-white text-xl mt-2.5">This example triggers the transfer method on the Fire Token Contract and creates an engagement if the Transfer event got triggered on the blockchain</p>
+
+              </div>
+
+
+            </div>
+
+          </div>
+
+        </header>
+        <div class="py-10">
+          <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-1 md:grid-cols-2 gap-5">
+            <!-- This example requires Tailwind CSS v2.0+ -->
+            <div class="bg-white shadow overflow-hidden sm:rounded-lg">
+              <div class="px-4 py-5 sm:px-6">
+                <h3 class="text-lg leading-6 font-medium text-gray-900">Condition</h3>
+                <p class="mt-1 max-w-2xl text-sm text-gray-500">Smart Contract Event</p>
+              </div>
+              <div class="border-t border-gray-200 px-4 py-5 sm:p-0">
+                <dl class="sm:divide-y sm:divide-gray-200">
+                  <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                    <dt class="text-sm font-medium text-gray-500">Blockchain</dt>
+                    <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">Matic Mainnet</dd>
+                  </div>
+                  <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                    <dt class="text-sm font-medium text-gray-500">Contract Address</dt>
+                    <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">0x502f6e93285aa37b2fc326e64ffc3d9eef47130d</dd>
+                  </div>
+                  <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                    <dt class="text-sm font-medium text-gray-500">Event</dt>
+                    <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">Transfer</dd>
+                  </div>
+                  <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                    <dt class="text-sm font-medium text-gray-500">Parameters</dt>
+                    <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                      <dl>
+                        <dd>From: null</dd>
+                        <dd>To: 0xB86E7b67ad427b2283849f846c17F29fA6f3af29</dd>
+                        <dd>Value: 1000000000000000000</dd>
+                      </dl>
+                    </dd>
+
+                  </div>
+
+                </dl>
+              </div>
+            </div>
+            <div class="bg-white shadow overflow-hidden sm:rounded-lg">
+              <div class="px-4 py-5 sm:px-6">
+                <h3 class="text-lg leading-6 font-medium text-gray-900">Action</h3>
+                <p class="mt-1 max-w-2xl text-sm text-gray-500">Smart Contract Method Action</p>
+              </div>
+              <div class="border-t border-gray-200 px-4 py-5 sm:p-0">
+                <dl class="sm:divide-y sm:divide-gray-200">
+                  <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                    <dt class="text-sm font-medium text-gray-500">Blockchain</dt>
+                    <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">Matic Mainnet</dd>
+                  </div>
+                  <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                    <dt class="text-sm font-medium text-gray-500">Contract Address</dt>
+                    <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">0x502f6e93285aa37b2fc326e64ffc3d9eef47130d</dd>
+                  </div>
+                  <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                    <dt class="text-sm font-medium text-gray-500">Method</dt>
+                    <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">transfer</dd>
+                  </div>
+                  <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4  sm:px-6">
+                    <dt class="text-sm font-medium text-gray-500">Parameters</dt>
+                    <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                      <dl>
+                        <dd>To: 0xB86E7b67ad427b2283849f846c17F29fA6f3af29</dd>
+                        <dd>Value: 1000000000000000000</dd>
+                      </dl>
+                    </dd>
+                  </div>
+
+                </dl>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+        <main class="-mt-32" id="app">
+          <div class="max-w-7xl mx-auto pb-12 px-4 sm:px-6 lg:px-8">
+            <div class="grid grid-cols-1 lg:grid-cols-3 gap-5">
+              <div class="col-span-1 lg:col-span-2">
+                <div class="bg-white rounded-lg shadow px-5 py-6 sm:px-6">
+                  <div class="bg-white py-5 border-b border-gray-200">
+                    <h3 class="text-lg leading-6 font-medium text-gray-900">Engagements</h3>
+                  </div>
+                  <!-- This example requires Tailwind CSS v2.0+ -->
+                  <div>
+                    <div class="flow-root mt-6">
+                      <ul role="list" class="-my-5 divide-y divide-gray-200">
+                        <li v-for="engagement in engagements" class="py-4" :key="engagement.id">
+                          <div class="flex items-center space-x-4">
+
+                            <div class="flex-1 min-w-0">
+                              <p class="text-sm font-medium text-gray-900 truncate">{{
+                                  engagement.transaction_hash
+                                }}</p>
+                            </div>
+                            <div>
+                              <a :href="'https://polygonscan.com/tx/'+engagement.transaction_hash"
+                                 target="_blank"
+                                 class="inline-flex items-center shadow-sm px-2.5 py-0.5 border border-gray-300 text-sm leading-5 font-medium rounded-full text-gray-700 bg-white hover:bg-gray-50">
+                                View </a>
+                            </div>
+                          </div>
+                        </li>
+                      </ul>
+                    </div>
+
+                  </div>
+
+                </div>
+              </div>
+              <div class="col-span-1 lg:col-span-1">
+
+                <div class="bg-white rounded-lg shadow px-5 py-6 sm:px-6">
+
+                  <div class="flex justify-between">
+                    <button
+                        v-if="!utility.selectedAccount"
+                        @click="connectWallet" type="button"
+                        class="w-full inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                      Connect Wallet
+                    </button>
+                    <span v-if="utility.selectedAccount && user.isValid"
+                          class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-indigo-100 text-indigo-800"> Already on Whitelist </span>
+
+                    <button
+                        v-if="utility.selectedAccount && !user.maxEngagementsReached"
+                        @click="triggerTransaction" type="button"
+                        class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                      Whitelist me
+                    </button>
+                  </div>
+                  <div v-if="selectedAccount" class="overflow-scroll my-3" >
+                    {{ selectedAccount }}
+                  </div>
+                </div>
+
+                <div class="bg-white rounded-lg shadow px-5 py-6 sm:px-6 mt-5">
+                  <div class="bg-white py-5 border-b border-gray-200">
+                    <h3 class="text-lg leading-6 font-medium text-gray-900">Logs</h3>
+                  </div>
+
+                  <p v-for="(entry, index) in log" v-html="entry" :key="'log'+index"></p>
+                </div>
+              </div>
+            </div>
+
+            <!-- /End replace -->
+          </div>
+        </main>
+      </div>
+
+  </main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,236 @@
+<script>
+
+
+export default {
+  data() {
+    return {
+
+    }
+  },
+  created() {
+
+
+
+  },
+}
+</script>
+
+<template>
+
+  <main>
+    <div class="min-h-full">
+      <div class="bg-gray-800 pb-32">
+        <nav class="bg-gray-800">
+          <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="border-b border-gray-700">
+              <div class="flex items-center justify-between h-16 px-4 sm:px-0">
+                <div class="flex items-center">
+                  <div class="flex-shrink-0">
+                    <img class="w-16" src="/logo.png"
+                         alt="Whal3s">
+                  </div>
+                </div>
+
+              </div>
+            </div>
+          </div>
+
+        </nav>
+        <header class="py-10">
+          <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 ">
+            <h1 class="text-4xl font-bold text-white">Whal3s demo</h1>
+            <p class="text-white text-xl">Explore several use cases and examples. More coming soon.</p>
+          </div>
+
+        </header>
+      </div>
+
+        <main class="-mt-32" id="app">
+          <div class="max-w-7xl mx-auto pb-12 px-4 sm:px-6 lg:px-8">
+
+
+            <div class="rounded-lg bg-gray-200 overflow-hidden shadow divide-y divide-gray-200 sm:divide-y-0 sm:grid sm:grid-cols-2 sm:gap-px">
+              <div class="rounded-tl-lg rounded-tr-lg sm:rounded-tr-none relative group bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500">
+                <div>
+      <span class="rounded-lg inline-flex p-3 bg-teal-50 text-teal-700 ring-4 ring-white">
+        <!-- Heroicon name: outline/clock -->
+        <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
+        </svg>
+      </span>
+                </div>
+                <div class="mt-8">
+                  <h3 class="text-lg font-medium">
+                    <a href="#/event-condition" class="focus:outline-none">
+                      <!-- Extend touch target to entire panel -->
+                      <span class="absolute inset-0" aria-hidden="true"></span>
+                      Onchain events
+                    </a>
+                  </h3>
+                  <p class="mt-2 text-sm text-gray-500">Trigger specific workflows based on onchain events</p>
+                </div>
+                <span class="pointer-events-none absolute top-6 right-6 text-gray-300 group-hover:text-gray-400" aria-hidden="true">
+      <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+        <path d="M20 4h1a1 1 0 00-1-1v1zm-1 12a1 1 0 102 0h-2zM8 3a1 1 0 000 2V3zM3.293 19.293a1 1 0 101.414 1.414l-1.414-1.414zM19 4v12h2V4h-2zm1-1H8v2h12V3zm-.707.293l-16 16 1.414 1.414 16-16-1.414-1.414z" />
+      </svg>
+    </span>
+              </div>
+
+              <div class="sm:rounded-tr-lg relative group bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500">
+                <div>
+      <span class="rounded-lg inline-flex p-3 bg-purple-50 text-purple-700 ring-4 ring-white">
+        <!-- Heroicon name: outline/badge-check -->
+        <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z" />
+        </svg>
+      </span>
+                </div>
+                <div class="mt-8">
+                  <h3 class="text-lg font-medium">
+                    <a  class="focus:outline-none">
+                      <!-- Extend touch target to entire panel -->
+                      <span class="absolute inset-0" aria-hidden="true"></span>
+                      Ownership validation
+                    </a>
+                  </h3>
+                  <p class="mt-2 text-sm text-gray-500">Ensure that your customers hold an unclaimed NFT in their wallet</p>
+                </div>
+                <span class="pointer-events-none absolute top-6 right-6 text-gray-300 group-hover:text-gray-400" aria-hidden="true">
+<!--      <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">-->
+<!--        <path d="M20 4h1a1 1 0 00-1-1v1zm-1 12a1 1 0 102 0h-2zM8 3a1 1 0 000 2V3zM3.293 19.293a1 1 0 101.414 1.414l-1.414-1.414zM19 4v12h2V4h-2zm1-1H8v2h12V3zm-.707.293l-16 16 1.414 1.414 16-16-1.414-1.414z" />-->
+<!--      </svg>-->
+                  Example coming soon
+    </span>
+              </div>
+
+              <div class="relative group bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500">
+                <div>
+      <span class="rounded-lg inline-flex p-3 bg-sky-50 text-sky-700 ring-4 ring-white">
+        <!-- Heroicon name: outline/users -->
+
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v12m-3-2.818l.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+</svg>
+
+      </span>
+                </div>
+                <div class="mt-8">
+                  <h3 class="text-lg font-medium">
+                    <a class="focus:outline-none">
+                      <!-- Extend touch target to entire panel -->
+                      <span class="absolute inset-0" aria-hidden="true"></span>
+                      Transaction validation
+                    </a>
+                  </h3>
+                  <p class="mt-2 text-sm text-gray-500">Track transactions to be sure, your customers paid the invoice
+                  </p>
+                </div>
+                <span class="pointer-events-none absolute top-6 right-6 text-gray-300 group-hover:text-gray-400" aria-hidden="true">
+<!--      <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">-->
+<!--        <path d="M20 4h1a1 1 0 00-1-1v1zm-1 12a1 1 0 102 0h-2zM8 3a1 1 0 000 2V3zM3.293 19.293a1 1 0 101.414 1.414l-1.414-1.414zM19 4v12h2V4h-2zm1-1H8v2h12V3zm-.707.293l-16 16 1.414 1.414 16-16-1.414-1.414z" />-->
+<!--      </svg>-->
+                   Example coming soon
+    </span>
+              </div>
+
+              <div class="relative group bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500">
+                <div>
+      <span class="rounded-lg inline-flex p-3 bg-yellow-50 text-yellow-700 ring-4 ring-white">
+        <!-- Heroicon name: outline/cash -->
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 6a7.5 7.5 0 107.5 7.5h-7.5V6z" />
+  <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 10.5H21A7.5 7.5 0 0013.5 3v7.5z" />
+</svg>
+
+      </span>
+                </div>
+                <div class="mt-8">
+                  <h3 class="text-lg font-medium">
+                    <a  class="focus:outline-none">
+                      <!-- Extend touch target to entire panel -->
+                      <span class="absolute inset-0" aria-hidden="true"></span>
+                      NFT trait validation
+
+                    </a>
+                  </h3>
+                  <p class="mt-2 text-sm text-gray-500">Select the your customers by limiting Utility to certain NFT traits.
+                  </p>
+                </div>
+                <span class="pointer-events-none absolute top-6 right-6 text-gray-300 group-hover:text-gray-400" aria-hidden="true">
+<!--      <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">-->
+<!--        <path d="M20 4h1a1 1 0 00-1-1v1zm-1 12a1 1 0 102 0h-2zM8 3a1 1 0 000 2V3zM3.293 19.293a1 1 0 101.414 1.414l-1.414-1.414zM19 4v12h2V4h-2zm1-1H8v2h12V3zm-.707.293l-16 16 1.414 1.414 16-16-1.414-1.414z" />-->
+<!--      </svg>-->
+                   Example coming soon
+    </span>
+              </div>
+
+              <div class="sm:rounded-bl-lg relative group bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500">
+                <div>
+      <span class="rounded-lg inline-flex p-3 bg-rose-50 text-rose-700 ring-4 ring-white">
+        <!-- Heroicon name: outline/receipt-refund -->
+       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941" />
+</svg>
+
+      </span>
+                </div>
+                <div class="mt-8">
+                  <h3 class="text-lg font-medium">
+                    <a class="focus:outline-none" disabled>
+                      <!-- Extend touch target to entire panel -->
+                      <span class="absolute inset-0" aria-hidden="true"></span>
+                      Value validation
+
+                    </a>
+                  </h3>
+                  <p class="mt-2 text-sm text-gray-500">Validate your customers to hold at least a minimum amount of your distributed tokens.</p>
+                </div>
+                <span class="pointer-events-none absolute top-6 right-6 text-gray-300 group-hover:text-gray-400" aria-hidden="true">
+<!--      <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">-->
+<!--        <path d="M20 4h1a1 1 0 00-1-1v1zm-1 12a1 1 0 102 0h-2zM8 3a1 1 0 000 2V3zM3.293 19.293a1 1 0 101.414 1.414l-1.414-1.414zM19 4v12h2V4h-2zm1-1H8v2h12V3zm-.707.293l-16 16 1.414 1.414 16-16-1.414-1.414z" />-->
+<!--      </svg>-->
+                  Example coming soon
+    </span>
+              </div>
+
+              <div class="rounded-bl-lg rounded-br-lg sm:rounded-bl-none relative group bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500">
+                <div>
+      <span class="rounded-lg inline-flex p-3 bg-indigo-50 text-indigo-700 ring-4 ring-white">
+        <!-- Heroicon name: outline/academic-cap -->
+        <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+          <path d="M12 14l9-5-9-5-9 5 9 5z" />
+          <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
+          <path stroke-linecap="round" stroke-linejoin="round" d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222" />
+        </svg>
+      </span>
+                </div>
+                <div class="mt-8">
+                  <h3 class="text-lg font-medium">
+                    <a  class="focus:outline-none">
+                      <!-- Extend touch target to entire panel -->
+                      <span class="absolute inset-0" aria-hidden="true"></span>
+                      And many more
+                    </a>
+                  </h3>
+                  <p class="mt-2 text-sm text-gray-500">These are just a few condition examples. Combined with many actions they provite unlimited utilities.</p>
+                </div>
+                <span class="pointer-events-none absolute top-6 right-6 text-gray-300 group-hover:text-gray-400" aria-hidden="true">
+<!--      <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">-->
+<!--        <path d="M20 4h1a1 1 0 00-1-1v1zm-1 12a1 1 0 102 0h-2zM8 3a1 1 0 000 2V3zM3.293 19.293a1 1 0 101.414 1.414l-1.414-1.414zM19 4v12h2V4h-2zm1-1H8v2h12V3zm-.707.293l-16 16 1.414 1.414 16-16-1.414-1.414z" />-->
+<!--      </svg>-->
+    </span>
+              </div>
+            </div>
+
+
+            <!-- /End replace -->
+          </div>
+        </main>
+      </div>
+
+  </main>
+</template>
+
+<style scoped>
+
+</style>
